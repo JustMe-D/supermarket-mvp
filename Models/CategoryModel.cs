058b9c7f819e4bf8555d21e07c7ec9225610f4f7
Miguel Angel Aguirre <miguelangelbernal09@gmail.com>
2025-04-09 23:55:58 -0500

    Create Category Model

---
+++ Models/CategoryModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Supermarket_mvp.Models
+{
+    internal class CategoryModel
+    {
+
+        [DisplayName("Category Id")]
+        public int Id { get; set; }
+
+        [DisplayName("Category Name")]
+        [Required(ErrorMessage = "Category name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Category name must be between 3 and 50 characters")]
+        public string Name { get; set; }
+
+        [DisplayName("Description")]
+        [StringLength(200, ErrorMessage = "Description cannot exceed 200 characters")]
+        public string Description { get; set; }
+
+    }
+}
