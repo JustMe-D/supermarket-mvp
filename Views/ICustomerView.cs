f9c97e09c64167e1619b372d02d9899eb1e317ac
Miguel Angel Aguirre <miguelangelbernal09@gmail.com>
2025-04-11 15:33:46 -0500

    Create Interface Customer View

---
+++ Views/ICustomerView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Views
+{
+    internal interface ICustomerView
+    {
+        string Customer_Id { get; set; }
+        string Customer_Document { get; set; }
+        string Customer_FirstName { get; set; }
+        string Customer_LastName { get; set; }
+        string Customer_Address { get; set; }
+        DateTime? Customer_Birthday { get; set; }
+        string Customer_PhoneNumber { get; set; }
+        string Customer_Email { get; set; }
+
+        event EventHandler SearchEvent;
+        event EventHandler AddNewEvent;
+        event EventHandler EditEvent;
+        event EventHandler DeleteEvent;
+        event EventHandler SaveEvent;
+        event EventHandler CancelEvent;
+
+        void SetCustomerListBindingSource(BindingSource customerList);
+        void Show();
+    }
+}
